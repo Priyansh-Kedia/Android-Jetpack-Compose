cd72e9f9a96ec074d50924949efb08641c3765da
Priyansh Kedia <kediapriyansh@gmail.com>
2022-08-20 00:53:03 +0530

    added conversation in compose

--- app/src/main/java/com/kedia/jetpack/MainActivity.kt
+++ app/src/main/java/com/kedia/jetpack/MainActivity.kt
@@ -3,37 +3,100 @@ package com.kedia.jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlin.random.Random
+import com.kedia.jetpack.ui.theme.JetpackExampleTheme
+
+data class Message(val author: String, val body: String)
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+           JetpackExampleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Conversation(messages = SampleData.conversationSample)
+                }
+           }
 
         }
     }
+
+    @Composable
+    fun Conversation(messages: List<Message>) {
+        LazyColumn {
+            items(messages) { message ->
+                MessageCard(message = message)
+            }
+        }
+    }
+
+    @Composable
+    fun MessageCard(message: Message) {
+        Row(
+            modifier = Modifier.padding(all = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile_picture),
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+            )
+            
+            Spacer(modifier = Modifier.width(8.dp))
+
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+            )
+            
+            Column(
+                modifier = Modifier
+                    .clickable {
+                        isExpanded = !isExpanded
+                    }
+            ) {
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Spacer(modifier = Modifier.width(4.dp))
+                
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    color = surfaceColor,
+                    modifier = Modifier.
+                            animateContentSize()
+                        .padding(1.dp)
+                ) {
+                    Text(
+                        text = message.body,
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                    )
+                }
+            }
+        }
+    }
+
 }
